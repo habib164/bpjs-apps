d82256c9feba72638021b395ee8ce5c67d0a6b5e
Pandu Prabu <xprbs.me@gmail.com>
2023-08-15 03:57:58 +0700

    current code

--- app/Pegawai/Notifikasi.js
+++ app/Pegawai/Notifikasi.js
@@ -1,94 +1,146 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
-	Text,
-	View,
-	SafeAreaView,
-	Image,
-	StyleSheet,
-	ScrollView,
-	TouchableOpacity,
+    Text,
+    View,
+    SafeAreaView,
+    Image,
+    StyleSheet,
+    ScrollView,
+    TouchableOpacity,
 } from "react-native";
-
+import { db } from "../../firebaseConfig";
+import {
+    getFirestore,
+    collection,
+    getDocs,
+    where,
+    query,
+    getDoc,
+    addDoc,
+} from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 const Notifikasi = () => {
-	return (
-		<SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
-			<ScrollView>
-				<Image source={require("../../logo-bpjs.png")} style={styles.img} />
-				<View style={{ paddingHorizontal: 20 }}>
-					<View style={styles.cardContainer}>
-						<View style={styles.rowTop}>
-							<Text>Nama barang</Text>
-							<Text>Tanggal Pengajuan</Text>
-						</View>
-						<View style={styles.underline} />
-						<View style={styles.rowName}>
-							<Text>Jumlah Barang</Text>
-							<Text style={styles.acc}>Status</Text>
-						</View>
-					</View>
-				</View>
-			</ScrollView>
-		</SafeAreaView>
-	);
+    const [data, setData] = useState();
+    const requestCollection = collection(db, "request_atk");
+    const getData = async () => {
+        const name = await AsyncStorage.getItem("nama_pegawai");
+        const npp = await AsyncStorage.getItem("npp");
+
+        const q = query(
+            requestCollection,
+            where("nama_pegawai", "==", name),
+            where("npp", "==", npp)
+        );
+        const querySnapshot = await getDocs(q);
+        const requestData = querySnapshot.docs.map((doc) => doc.data());
+        console.log(requestData);
+        setData(requestData);
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+            <ScrollView>
+                <Image
+                    source={require("../../logo-bpjs.png")}
+                    style={styles.img}
+                />
+                <View style={{ paddingHorizontal: 20, gap: 20 }}>
+                    {data &&
+                        data.map((request, index) => (
+                            <View key={index} style={styles.cardContainer}>
+                                <View style={styles.rowTop}>
+                                    <Text>{request.nama_barang}</Text>
+                                    <Text>
+                                        {request.nama_pegawai}
+                                        {new Date(
+                                            request.tanggal_pengajuan.seconds *
+                                                1000
+                                        ).toLocaleDateString("id-ID")}
+                                    </Text>
+                                </View>
+                                <View style={styles.underline} />
+                                <View style={styles.rowName}>
+                                    <Text>{request.jumlah}</Text>
+                                    <Text
+                                        style={
+                                            request.status === "accept"
+                                                ? styles.acc
+                                                : request.status === "reject"
+                                                ? styles.reject
+                                                : styles.pending
+                                        }
+                                    >
+                                        {request.status}
+                                    </Text>
+                                </View>
+                            </View>
+                        ))}
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
 };
 
 export default Notifikasi;
 
 const styles = StyleSheet.create({
-	acc: {
-		color: "#22C55E",
-	},
-	pending: {
-		color: "#EAB308",
-	},
-	reject: {
-		color: "#EF4444",
-	},
-	img: {
-		width: 280,
-		height: 43,
-		margin: 30,
-		marginBottom: 30,
-		alignSelf: "center",
-	},
-	cardContainer: {
-		borderWidth: 1,
-		borderColor: "#CBD5E1",
-		paddingHorizontal: 20,
-		paddingVertical: 12,
-		borderRadius: 5,
-	},
-	rowTop: {
-		flexDirection: "row",
-		justifyContent: "space-between",
-		alignItems: "center",
-	},
-	underline: {
-		borderWidth: 0.8,
-		borderColor: "#CBD5E1",
-		marginVertical: 5,
-	},
-	rowBtn: {
-		flexDirection: "row",
-		justifyContent: "space-between",
-		marginTop: 10,
-	},
-	btnAcc: {
-		padding: 10,
-		backgroundColor: "#22C55E",
-		borderRadius: 10,
-		alignItems: "center",
-		width: "46%",
-	},
-	btnReject: {
-		padding: 10,
-		backgroundColor: "#EF4444",
-		borderRadius: 10,
-		alignItems: "center",
-		width: "46%",
-	},
-	rowName: {
-		flexDirection: "row",
-		justifyContent: "space-between",
-	},
+    acc: {
+        color: "#22C55E",
+    },
+    pending: {
+        color: "#EAB308",
+    },
+    reject: {
+        color: "#EF4444",
+    },
+    img: {
+        width: 280,
+        height: 43,
+        margin: 30,
+        marginBottom: 30,
+        alignSelf: "center",
+    },
+    cardContainer: {
+        borderWidth: 1,
+        borderColor: "#CBD5E1",
+        paddingHorizontal: 20,
+        paddingVertical: 12,
+        borderRadius: 5,
+    },
+    rowTop: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    underline: {
+        borderWidth: 0.8,
+        borderColor: "#CBD5E1",
+        marginVertical: 5,
+    },
+    rowBtn: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 10,
+    },
+    btnAcc: {
+        padding: 10,
+        backgroundColor: "#22C55E",
+        borderRadius: 10,
+        alignItems: "center",
+        width: "46%",
+    },
+    btnReject: {
+        padding: 10,
+        backgroundColor: "#EF4444",
+        borderRadius: 10,
+        alignItems: "center",
+        width: "46%",
+    },
+    rowName: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
 });
--- app/Pegawai/NotifikasiAdmin.js
+++ app/Pegawai/NotifikasiAdmin.js
@@ -1,96 +1,180 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
-	Text,
-	View,
-	SafeAreaView,
-	Image,
-	StyleSheet,
-	ScrollView,
-	TouchableOpacity,
+    Text,
+    View,
+    SafeAreaView,
+    Image,
+    StyleSheet,
+    ScrollView,
+    TouchableOpacity,
 } from "react-native";
-
+import { db } from "../../firebaseConfig";
+import {
+    getFirestore,
+    collection,
+    getDocs,
+    where,
+    query,
+    getDoc,
+    addDoc,
+    doc,
+    updateDoc,
+} from "firebase/firestore";
 const NotifikasiAdmin = () => {
-	return (
-		<SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
-			<ScrollView>
-				<Image source={require("../../logo-bpjs.png")} style={styles.img} />
-				<View style={{ paddingHorizontal: 20 }}>
-					<View style={styles.cardContainer}>
-						<View style={styles.rowTop}>
-							<View>
-								<Text>Nama : Gatot</Text>
-								<Text>NPP : 1234</Text>
-							</View>
-							<Text>Tanggal Pengajuan</Text>
-						</View>
-						<View style={styles.underline} />
-						<View style={styles.rowName}>
-							<Text>Nama Barang</Text>
-							<Text>Jumlah Barang</Text>
-						</View>
-						<View style={styles.rowBtn}>
-							<TouchableOpacity style={styles.btnAcc}>
-								<Text style={{ color: "white" }}>Terima</Text>
-							</TouchableOpacity>
-							<TouchableOpacity style={styles.btnReject}>
-								<Text style={{ color: "white" }}>Tolak</Text>
-							</TouchableOpacity>
-						</View>
-					</View>
-				</View>
-			</ScrollView>
-		</SafeAreaView>
-	);
+    const [data, setData] = useState();
+    const [isSuccess, setSuccess] = useState(false);
+    const requestCollection = collection(db, "request_atk");
+
+    const getData = async () => {
+        const q = query(requestCollection);
+        const querySnapshot = await getDocs(q);
+        const requestData = querySnapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }));
+        // console.log(requestData);
+        setData(requestData);
+    };
+
+    useEffect(() => {
+        getData();
+    }, [isSuccess]);
+
+    const handleReject = async (id) => {
+        const docRef = doc(db, "request_atk", id);
+        try {
+            await updateDoc(docRef, { status: "reject" });
+            setSuccess(true);
+        } catch (error) {
+            console.error("Error rejecting request:", error);
+        }
+    };
+
+    const handleAccept = async (id) => {
+        const docRef = doc(db, "request_atk", id);
+        try {
+            await updateDoc(docRef, { status: "accept" });
+            setSuccess(true);
+        } catch (error) {
+            console.error("Error rejecting request:", error);
+        }
+    };
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+            <ScrollView>
+                <Image
+                    source={require("../../logo-bpjs.png")}
+                    style={styles.img}
+                />
+                <View style={{ paddingHorizontal: 20, gap: 20 }}>
+                    {data &&
+                        data.map((item, index) => (
+                            <View style={styles.cardContainer}>
+                                <View style={styles.rowTop}>
+                                    <View>
+                                        <Text>Nama : {item.nama_pegawai}</Text>
+                                        <Text>NPP : {item.npp}</Text>
+                                    </View>
+                                    <Text>
+                                        {" "}
+                                        {new Date(
+                                            item.tanggal_pengajuan.seconds *
+                                                1000
+                                        ).toLocaleDateString("id-ID")}
+                                    </Text>
+                                </View>
+                                <View style={styles.underline} />
+                                <View style={styles.rowName}>
+                                    <Text>{item.nama_barang}</Text>
+                                    <Text>{item.jumlah}</Text>
+                                </View>
+                                <View style={styles.rowBtn}>
+                                    {item.status === "pending" && (
+                                        <>
+                                            <TouchableOpacity
+                                                style={styles.btnAcc}
+                                                onPress={() =>
+                                                    handleAccept(item.id)
+                                                }
+                                            >
+                                                <Text
+                                                    style={{ color: "white" }}
+                                                >
+                                                    Terima
+                                                </Text>
+                                            </TouchableOpacity>
+                                            <TouchableOpacity
+                                                style={styles.btnReject}
+                                                onPress={() =>
+                                                    handleReject(item.id)
+                                                }
+                                            >
+                                                <Text
+                                                    style={{ color: "white" }}
+                                                >
+                                                    Tolak
+                                                </Text>
+                                            </TouchableOpacity>
+                                        </>
+                                    )}
+                                </View>
+                            </View>
+                        ))}
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
 };
 
 export default NotifikasiAdmin;
 
 const styles = StyleSheet.create({
-	img: {
-		width: 280,
-		height: 43,
-		margin: 30,
-		marginBottom: 30,
-		alignSelf: "center",
-	},
-	cardContainer: {
-		borderWidth: 1,
-		borderColor: "#CBD5E1",
-		paddingHorizontal: 20,
-		paddingVertical: 12,
-		borderRadius: 5,
-	},
-	rowTop: {
-		flexDirection: "row",
-		justifyContent: "space-between",
-		alignItems: "center",
-	},
-	underline: {
-		borderWidth: 0.8,
-		borderColor: "#CBD5E1",
-		marginVertical: 5,
-	},
-	rowBtn: {
-		flexDirection: "row",
-		justifyContent: "space-between",
-		marginTop: 10,
-	},
-	btnAcc: {
-		padding: 10,
-		backgroundColor: "#22C55E",
-		borderRadius: 10,
-		alignItems: "center",
-		width: "46%",
-	},
-	btnReject: {
-		padding: 10,
-		backgroundColor: "#EF4444",
-		borderRadius: 10,
-		alignItems: "center",
-		width: "46%",
-	},
-	rowName: {
-		flexDirection: "row",
-		justifyContent: "space-between",
-	},
+    img: {
+        width: 280,
+        height: 43,
+        margin: 30,
+        marginBottom: 30,
+        alignSelf: "center",
+    },
+    cardContainer: {
+        borderWidth: 1,
+        borderColor: "#CBD5E1",
+        paddingHorizontal: 20,
+        paddingVertical: 12,
+        borderRadius: 5,
+    },
+    rowTop: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    underline: {
+        borderWidth: 0.8,
+        borderColor: "#CBD5E1",
+        marginVertical: 5,
+    },
+    rowBtn: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 10,
+    },
+    btnAcc: {
+        padding: 10,
+        backgroundColor: "#22C55E",
+        borderRadius: 10,
+        alignItems: "center",
+        width: "46%",
+    },
+    btnReject: {
+        padding: 10,
+        backgroundColor: "#EF4444",
+        borderRadius: 10,
+        alignItems: "center",
+        width: "46%",
+    },
+    rowName: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
 });
